59ad0bd1bbd30a732c6e5369166f8af7c0445317
unknown <tta2yta@gmail.com>
2020-11-06 11:50:56 +0300

    adding form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import GitUsers from './components/GitUsers';
 
 function App() {
   return (
     <div className="App">
-     
+     <GitUsers />
     </div>
   );
 }
--- src/components/GitUsers.js
+++ src/components/GitUsers.js
@@ -2,15 +2,20 @@ import React, {useState, useEffect} from 'react'
 
 export default function GitUsers() {
 
+    const[gitUsers, setGitUsers]=useState([]);
+
 useEffect(()=>{
-    fetch("https://api.github.com/users/example")
-    .then(response=>response.json)
-    .then(data=>console.log(data))
-})
+    fetch("https://api.github.com/users/tta2yta")
+    .then(response=>response.json())
+    .then(data=>{console.log(data); setGitUsers(data)});
+},[])
 
     return (
         <div>
-            
+            {console.log(gitUsers)}
+            <form>
+                <label>Please Enter Github UserName</label><input type="text" value="" onChange="" />
+            </form>
         </div>
     )
 }
